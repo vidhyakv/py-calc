ed0626098c1308eb2cad7a9f3c7eb0930ec12400
vidhyakv <vidhyapriyaece@gmail.com>
2025-09-14 11:59:28 +0530

    hanldes sum of two comma-separated numbers

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -2,6 +2,7 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers == "":
             return 0
-        if numbers.isdigit():
-            return int(numbers)
-        return -1
+        parts = numbers.split(",")
+        if len(parts) == 1:
+            return int(parts[0])
+        return int(parts[0]) + int(parts[1])
