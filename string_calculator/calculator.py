96fd5e439c83b443d02181cdd11869d00005a012
vidhyakv <vidhyapriyaece@gmail.com>
2025-09-14 14:25:38 +0530

    handles multiple delimiters group inside square brackets

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,3 +1,4 @@
+import re
 class StringCalculator:
     MAX_NUMBER = 1000
     DEFAULT_DELIMITER = ","
@@ -11,14 +12,19 @@ class StringCalculator:
         if not numbers:
             return 0
 
-        delimiter = self.DEFAULT_DELIMITER
+        delimiters = [self.DEFAULT_DELIMITER]
 
         if numbers.startswith(self.CUSTOM_DELIMITER_PREFIX):
             delimiter_definition, numbers = numbers.split(self.NEWLINE, 1)
-            delimiter = delimiter_definition[len(self.CUSTOM_DELIMITER_PREFIX):]
+            if delimiter_definition.startswith("//["):
+                delimiters = re.findall(r"\[(.*?)]", delimiter_definition[len(self.CUSTOM_DELIMITER_PREFIX):])
+            else:
+                delimiters = [delimiter_definition[len(self.CUSTOM_DELIMITER_PREFIX):]]
 
-        numbers_with_delimiter = numbers.replace(self.NEWLINE, delimiter)
-        number_strings = numbers_with_delimiter.split(delimiter)
+        numbers_with_delimiter = numbers.replace(self.NEWLINE, self.DEFAULT_DELIMITER)
+        pattern = "|".join(map(re.escape, delimiters))
+
+        number_strings = re.split(pattern,numbers_with_delimiter)
 
         number_values = [num for value in number_strings if (num := int(value)) <= self.MAX_NUMBER]
 
