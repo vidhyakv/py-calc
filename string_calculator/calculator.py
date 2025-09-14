6149071d53a4af63abfd34d8fe5c845ff0e9cf2c
vidhyakv <vidhyapriyaece@gmail.com>
2025-09-14 11:55:51 +0530

    handle single number input

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -2,4 +2,6 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers == "":
             return 0
+        if numbers.isdigit():
+            return int(numbers)
         return -1
