db7730d249740161665b5b8a2fef699bf250db3c
vidhyakv <vidhyapriyaece@gmail.com>
2025-09-14 12:15:16 +0530

    test case for supporting custom delimiter

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -28,5 +28,9 @@ def test_newline_as_delimiter(sc):
     assert sc.add("1\n2,3") == 6
     assert sc.add("10\n20\n30") == 60
 
+def test_custom_delimiter(sc):
+    assert sc.add("//;\n1;2") == 3
+    assert sc.add("//|\n4|5|6") == 15
+
 
 
