4685f6432cb8e72abc9b484071e9bbc6ac46d7b1
vidhyakv <vidhyapriyaece@gmail.com>
2025-09-14 12:07:21 +0530

    refactors code

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -25,9 +25,4 @@ def test_multiple_numbers_returns_sum(sc):
     assert sc.add("5,5,5,5") == 20
 
 
-def test_newline_as_delimiter(sc):
-    assert sc.add("1\n2,3") == 6
-    assert sc.add("10\n20\n30") == 60
-
-
 
