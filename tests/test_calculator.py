f7fd0e74fb143528a315c2b47e7a74fac880b891
vidhyakv <vidhyapriyaece@gmail.com>
2025-09-14 12:06:18 +0530

    refactors code

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -20,9 +20,14 @@ def test_two_numbers_returns_sum(sc):
     assert sc.add("1,2") == 3
     assert sc.add("22,43") == 65
 
-def test_multiple_numbers_returns_sum():
-    calc = StringCalculator()
-    assert calc.add("1,2,3") == 6
-    assert calc.add("5,5,5,5") == 20
+def test_multiple_numbers_returns_sum(sc):
+    assert sc.add("1,2,3") == 6
+    assert sc.add("5,5,5,5") == 20
+
+
+def test_newline_as_delimiter(sc):
+    assert sc.add("1\n2,3") == 6
+    assert sc.add("10\n20\n30") == 60
+
 
 
