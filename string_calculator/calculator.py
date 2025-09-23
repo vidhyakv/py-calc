68038e27ade1a07e892de2bd37be36a05732c4af
vidhyakv <vidhyapriyaece@gmail.com>
2025-09-23 18:50:14 +0530

    extracting number function

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,6 +1,37 @@
 import re
 
 
+def handle_negatives(number_values):
+    negatives = [x for x in number_values if x < 0]
+    if negatives:
+        raise ValueError(f"Negatives not allowed: {negatives}")
+
+
+def extract_delimiters(numbers):
+    MAX_NUMBER = 1000
+    DEFAULT_DELIMITER = ","
+    NEWLINE = "\n"
+    CUSTOM_DELIMITER_PREFIX = "//"
+    MULTI_DELIMITER_PREFIX = "//["
+    MULTI_DELIMITER_REGEX = r"\[(.*?)]"
+    DEFAULT_DELIMITER = ","
+
+
+    delimiters = [DEFAULT_DELIMITER]
+
+    if numbers.startswith(CUSTOM_DELIMITER_PREFIX):
+        delimiter_definition, numbers = numbers.split(NEWLINE, 1)
+        custom_delimiters = delimiter_definition[len(CUSTOM_DELIMITER_PREFIX):]
+
+        if delimiter_definition.startswith(MULTI_DELIMITER_PREFIX):
+            delimiters = re.findall(MULTI_DELIMITER_REGEX, custom_delimiters)
+        else:
+            delimiters = [custom_delimiters]
+
+
+    return delimiters
+
+
 class StringCalculator:
     MAX_NUMBER = 1000
     DEFAULT_DELIMITER = ","
@@ -16,17 +47,9 @@ class StringCalculator:
         if not numbers:
             return 0
 
-        delimiters = [self.DEFAULT_DELIMITER]
+        delimiters = extract_delimiters(numbers)
 
-        if numbers.startswith(self.CUSTOM_DELIMITER_PREFIX):
-            delimiter_definition, numbers = numbers.split(self.NEWLINE, 1)
-            custom_delimiters = delimiter_definition[len(self.CUSTOM_DELIMITER_PREFIX):]
-
-            if delimiter_definition.startswith(self.MULTI_DELIMITER_PREFIX):
-                delimiters = re.findall(self.MULTI_DELIMITER_REGEX, custom_delimiters)
-            else:
-                delimiters = [custom_delimiters]
-
+        print(delimiters)
         numbers_with_delimiter = numbers.replace(self.NEWLINE, self.DEFAULT_DELIMITER)
         pattern = "|".join(map(re.escape, delimiters))
 
@@ -37,9 +60,5 @@ class StringCalculator:
             for value in number_strings
             if value.lstrip('-').isdigit() and int(value) <= self.MAX_NUMBER
         ]
-
-        negatives = [x for x in number_values if x < 0]
-        if negatives:
-            raise ValueError(f"Negatives not allowed: {negatives}")
-
+        handle_negatives(number_values)
         return sum(number_values)
