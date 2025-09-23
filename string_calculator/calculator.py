fa58bf8d1db1a83381de88b0d4b8204fe4367cc7
vidhyakv <vidhyapriyaece@gmail.com>
2025-09-23 19:29:25 +0530

    refactors  - use class level variables and make all funcs private and depends on class instance

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -12,38 +12,38 @@ class StringCalculator:
         self._numbers_str = ""
         self._numbers = []
 
-    @staticmethod
-    def _handle_negatives(number_values):
-        negatives = [x for x in number_values if x < 0]
+    def _handle_negatives(self):
+        negatives = [x for x in self._numbers if x < 0]  # check parsed numbers, not string
         if negatives:
             raise ValueError(f"Negatives not allowed: {negatives}")
 
-    @classmethod
-    def _extract_delimiters(cls, numbers):
-        if numbers.startswith(cls.MULTI_DELIMITER_PREFIX):
-            return re.findall(cls.MULTI_DELIMITER_REGEX, numbers)
-        elif numbers.startswith(cls.CUSTOM_DELIMITER_PREFIX):
-            return [numbers[2]]
-        return [cls.DEFAULT_DELIMITER]
+    def _extract_delimiters(self):
+        if self._numbers_str.startswith(self.MULTI_DELIMITER_PREFIX):
+            return re.findall(self.MULTI_DELIMITER_REGEX, self._numbers_str)
+        elif self._numbers_str.startswith(self.CUSTOM_DELIMITER_PREFIX):
+            return [self._numbers_str[2]]
+        return [self.DEFAULT_DELIMITER]
 
-    def add(self, numbers: str) -> int:
-        if not numbers:
+    def _remove_header(self):
+        if self._numbers_str.startswith(self.CUSTOM_DELIMITER_PREFIX):
+            self._numbers_str = self._numbers_str.split(self.NEWLINE, 1)[1]
+
+    def add(self, numbers_str: str) -> int:
+        if not numbers_str:
             return 0
 
-        delimiters = self._extract_delimiters(numbers)
+        self._numbers_str = numbers_str
+        delimiters = self._extract_delimiters()
+        self._remove_header()
 
-        if numbers.startswith(self.CUSTOM_DELIMITER_PREFIX):
-            numbers = numbers.split(self.NEWLINE, 1)[1]
-
-        numbers_with_delimiter = numbers.replace(self.NEWLINE, self.DEFAULT_DELIMITER)
+        numbers_with_delimiter = self._numbers_str.replace(self.NEWLINE, self.DEFAULT_DELIMITER)
         pattern = "|".join(map(re.escape, delimiters))
 
-        number_strings = re.split(pattern, numbers_with_delimiter)
-
-        number_values = [
+        self._numbers = [
             int(value)
-            for value in number_strings
+            for value in re.split(pattern, numbers_with_delimiter)
             if value.lstrip('-').isdigit() and int(value) <= self.MAX_NUMBER
         ]
-        self._handle_negatives(number_values)
-        return sum(number_values)
+
+        self._handle_negatives()
+        return sum(self._numbers)
